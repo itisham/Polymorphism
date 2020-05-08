26d886ce22958d9a391fa326e9e488ddcc490826
itisham <61932014+itisham@users.noreply.github.com>
2020-05-08 22:48:32 +0700

    Add files via upload
    Polymorphism Menggunakan Inheritance/Pewarisan

---
+++ PrinterWindows.cs
@@ -0,0 +1,21 @@
+﻿using Polymorphism_Menggunakan_Inheritance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism_Menggunakan_Inheritance
+{
+    public class PrinterWindows
+    {
+        public virtual void Show()
+        {
+            Console.WriteLine("\nPrinter display dimension");
+        }
+        public virtual void Print()
+        {
+            Console.WriteLine("printer printing");
+        }
+    }
+}
